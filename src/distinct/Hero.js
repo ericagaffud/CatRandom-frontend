2029082503b7ac07c606f6564bd699c4459c98f2
JDen21 <garciaden492@gmail.com>
2022-08-26 17:51:14 +0800

    hero animation

--- src/distinct/Hero.js
+++ src/distinct/Hero.js
@@ -1,4 +1,9 @@
-import network from '../images/Network.png'
+import network from '../images/frame.svg'
+import man1 from '../images/man1.svg'
+import man2 from '../images/man2.svg'
+import lady1 from '../images/lady1.svg'
+import lady2 from '../images/lady2.svg'
+import lady3 from '../images/lady3.svg'
 import style from './Hero.module.css'
 export default function Hero(){
     return(
@@ -8,8 +13,13 @@ export default function Hero(){
           <button className={`${style.transparent}`}> SignUp / Login </button>
           <button> Start Chatting </button>
         </div>
-        <div>
-          <img src={network} alt="Networks" className={`${style.network}`}></img>
+        <div className={`${style.network}`} >
+          <img src={network} alt="Networks" ></img>
+          <img src={man1} className={`${style.avatar} ${style.man1}`} alt="man in black clothes" />
+          <img src={man2} className={`${style.avatar} ${style.man2}`} alt="another man" />
+          <img src={lady1} className={`${style.avatar} ${style.lady1} `} alt="lady1" />
+          <img src={lady2} className={`${style.avatar} ${style.lady2}`} alt="lady2" />
+          <img src={lady3} className={`${style.avatar} ${style.lady3}`} alt="lady3" />
         </div>
       </div>
     )
